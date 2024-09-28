0a6eb467e3d4109cbbbe1420cbf58e44576f4b77
Anjalit9714 <anjalit9714@gmail.com>
2024-09-28 11:58:37 +0530

    add

---
+++ largestNumber.js
@@ -0,0 +1,9 @@
+let largestNumber=function(nums){
+// a=nums.reverse();
+// b=a.join("");
+// return b
+
+
+
+}
+largestNumber([10,2])
---
+++ sortedSquares.js
@@ -0,0 +1,8 @@
+let sortedSquares=function(nums){
+a=nums.map(num => Math.pow(num,2));
+b=a.sort((a,b) => a-b);
+console.log(b);
+
+
+}
+sortedSquares([-7,-3,2,3,11]);
