78399c0a976fac5f9ba8f26d41353f19b9d40fb5
Anjalit9714 <anjalit9714@gmail.com>
2024-10-03 10:12:08 +0530

    new program

---
+++ singleNumber.js
@@ -0,0 +1,16 @@
+let singleNumber=function (nums) {
+   
+    nums.sort((a, b) => a - b);
+    
+    
+    for (let i = 0; i < nums.length; i += 2) {
+ 
+        if (i === nums.length - 1 || nums[i] !== nums[i + 1]) {
+            return nums[i];
+        }
+    }
+ }
+ 
+ console.log(singleNumber([4,1,2,1,2]));
+ 
+    
