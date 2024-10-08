a21f2643df5a27793cbac9a174b9109687de1b17
Anjalit9714 <anjalit9714@gmail.com>
2024-10-08 16:52:31 +0530

    Logical

---
+++ SearchInsertPosition.js
@@ -0,0 +1,8 @@
+const SearchInsertPosition = function (nums , target){
+    if(nums.includes(target)){
+        console.log(nums.indexOf(target));
+    }else {
+        if
+    }
+}
+SearchInsertPosition([1,3,5,6] , 2);
