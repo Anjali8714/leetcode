34c1e81529e0dd0976db1b9fa1cba036662667ec
Anjalit9714 <anjalit9714@gmail.com>
2024-10-10 10:32:28 +0530

    add

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,18 +1,33 @@
-function ContainsDuplicate(nums){
+// function ContainsDuplicate(nums){
 
-  for(let val of nums){
-    res=nums.indexOf(val);
-    result=nums.lastIndexOf(val);
-    if(res!==result)
-       {
-           return true
+//   for(let val of nums){
+//     res=nums.indexOf(val);
+//     result=nums.lastIndexOf(val);
+//     if(res!==result)
+//        {
+//            return true
             
-        }
-     }
-    return false
+//         }
+//      }
+//     return false
     
+// }
+// console.log(ContainsDuplicate([1,2,3,1]));
+
+function ContainsDuplicate(nums){
+   res= nums.sort((a,b) => a-b);
+   for(i=1;i<res.length;i++){
+    if(res[i] === res[i-1]){
+        return true
+    }
+   
+   }
+   return false
 }
+
+
 console.log(ContainsDuplicate([1,2,3,1]));
+
 
      
     
