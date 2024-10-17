316778a53015ac81af5e02205ecef80eadfcdff1
Anjalit9714 <anjalit9714@gmail.com>
2024-10-17 10:25:42 +0530

    add

---
+++ RemoveElements.js
@@ -0,0 +1,16 @@
+const RemoveElement =(nums,val) => {
+    let k = 0; 
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++; 
+        }
+    }
+    
+    return k; 
+}
+
+console.log(RemoveElement([3,2,2,3],3));
+
+
