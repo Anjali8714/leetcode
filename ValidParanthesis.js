3af3de56d2612cdb4af52830efc84ce8028981fb
Anjalit9714 <anjalit9714@gmail.com>
2024-10-25 10:57:56 +0530

    add

---
+++ ValidParanthesis.js
@@ -0,0 +1,29 @@
+const ValidParanthesis = (s) => {
+    let splitedvalue = s.split("");
+    let emptyarr=[];
+for(i=0;i<splitedvalue.length;i++){
+    if(splitedvalue[i]==='('|| splitedvalue[i]==='{' ||  splitedvalue[i]==='[' ){
+        emptyarr.push(splitedvalue[i]);
+    }
+    else if(splitedvalue[i] === ')' && emptyarr[emptyarr.length-1] ==='(' || splitedvalue[i] === '}' && emptyarr[emptyarr.length-1] ==='{' || splitedvalue[i] === ']' && emptyarr[emptyarr.length-1] ==='[' ){
+            emptyarr.pop();
+           
+        }
+       else{
+        return false
+       }
+}
+       return emptyarr.length==0
+    
+    
+}
+
+
+       
+        
+ 
+ 
+}
+
+console.log(ValidParanthesis("{()"));
+
