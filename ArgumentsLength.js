ef5a6b7f2d937fd6b539579c2a2e29375bfbffad
Anjalit9714 <anjalit9714@gmail.com>
2024-11-02 09:35:49 +0530

    add

---
+++ ArgumentsLength.js
@@ -0,0 +1,7 @@
+const ArgumentsLength = (args) => {
+    let result = args.length;
+    console.log(result);
+    
+}
+
+ArgumentsLength([5]);
---
+++ Triangle.js
@@ -0,0 +1,8 @@
+let array = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]];
+
+let sortedArray = array.map(subArray => [...subArray].sort((a, b) => a - b));
+for(i=0 ; i<sortedArray.length ; i++){
+    console.log(sortedArray[]);
+    
+}
+console.log(sortedArray);
