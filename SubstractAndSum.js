3590226225beec60856595c62b4b35c3ae8fb0b5
Anjalit9714 <anjalit9714@gmail.com>
2024-11-13 10:17:26 +0530

    add

---
+++ SubstractAndSum.js
@@ -0,0 +1,6 @@
+const n= 234;
+let splited = String(n).split('').map(Number);
+let product = splited.reduce((acc,item) => acc*item);
+let sum = splited.reduce((acc,item) => acc+item);
+let result = product-sum;
+console.log(result);
