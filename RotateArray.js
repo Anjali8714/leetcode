74dbc16572ffad3069b9d1b948c18e0eb1c8d6a5
Anjalit9714 <anjalit9714@gmail.com>
2024-11-19 10:02:53 +0530

    add

--- RotateArray.js
+++ RotateArray.js
@@ -1,8 +1,24 @@
 const nums=[1,2,3,4,5,6,7];
 const k=3;
-for(i=0;i<k;i++){
 
-   console.log(nums[i]); 
-   
+let n = nums.length;
+let w = k % n;
+
+function reverse(arr , start ,end){
+   while(start<end){
+      let temp=arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+   }
 }
+reverse(nums,0,n-1)
+reverse(nums,0,w-1)
+reverse(nums,w,n-1);
+console.log(nums);
+
+
+   
+
 
