684a89cfb149424ee5c7c70c10fa457ff3e013d9
Anjalit9714 <anjalit9714@gmail.com>
2024-11-23 09:45:15 +0530

    add

---
+++ ArrayReduceTransformation.js
@@ -0,0 +1,12 @@
+var reduce = function (nums, fn, init) {
+    let sum = init;
+    if (nums.length === 0) {
+        return init
+    }
+    for (i = 0; i < nums.length; i++) {
+        sum = fn(sum,nums[i])
+    }
+    return sum
+}
+
+console.log(reduce([1,2,3,4] , function sum(accum, curr) { return accum + curr; } , 0));
