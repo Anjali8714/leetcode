9b752d0e5fe86e5b00e2e19bd8efba718c7d750c
Anjalit9714 <anjalit9714@gmail.com>
2024-12-27 10:00:51 +0530

    add

---
+++ arrayWrapper.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+}
+console.log(ArrayWrapper([[1,2],[3,4]]));
+
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    if(this.nums.length == 0){
+        return 0
+    }
+    let sum =0;
+    for(let i of this.nums){
+        sum+=i;
+    }
+    return sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.nums)
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
