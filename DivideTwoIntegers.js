7bb5bd9e8c1dde0836812a35e4ac080b237da5cd
Anjalit9714 <anjalit9714@gmail.com>
2025-01-02 09:47:28 +0530

    add

--- DivideTwoIntegers.js
+++ DivideTwoIntegers.js
@@ -1,6 +1,11 @@
 const DivideTwoIntegers = (dividend , divisor) => {
-    let divide=dividend/divisor;
-    let result =Math.trunc(divide);
-    return result
+   
+        let divide=dividend/divisor;
+        let result =Math.trunc(divide);
+        if (divisor === -1) {
+            return (dividend === -2147483648) ? 2147483647 : -dividend;
+        }
+        return result
+    
 }
 console.log(DivideTwoIntegers(7 , -3));
